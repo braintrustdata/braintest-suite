2e76f5374fd49eb6ec40356d759fc57b4bbf31d0
Yash Vedanaparti <60951388+yashv19@users.noreply.github.com>
2026-02-09 15:51:28 -0500

    Removed time.sleep in mocks, which was messing with gevent

--- loadtest/mock_default_task.py
+++ loadtest/mock_default_task.py
@@ -18,7 +18,7 @@ def _mock_llm(
         )
 
     response = " ".join([fake.sentence() for _ in range(num_sentences)])
-    time.sleep(random.randint(0,2))
+    # time.sleep(random.randint(0,2))
     return response
 
 @traced
